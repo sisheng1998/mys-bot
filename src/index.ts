856d51879421f746e37acd19b91f88802b7d89c0
Sheng <sisheng1998@gmail.com>
2026-07-19 16:40:29 +0800

    feat(app): add app url

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { handleTextMessage } from "@/text"
 import { getUserIds } from "@/utils"
 
 interface Env {
+  APP_URL: string
   BOT_INFO: UserFromGetMe
   BOT_TOKEN: string
   ALLOWED_USER_IDS: string
@@ -14,6 +15,8 @@ interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    if (request.method === "GET") return Response.redirect(env.APP_URL, 302)
+
     const bot = new Bot(env.BOT_TOKEN, { botInfo: env.BOT_INFO })
 
     bot.use(isAuthorized(getUserIds(env.ALLOWED_USER_IDS)))
